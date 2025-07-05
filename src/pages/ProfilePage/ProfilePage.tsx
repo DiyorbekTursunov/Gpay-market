45b3c11ad990ae7e11646282f3b7549e2e182b7c
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2025-07-06 02:09:01 +0500

    fixed ui responsive

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -4,32 +4,36 @@ import ProfileRight from "../../components/ProfileRight/ProfileRight";
 import AtomicPng from "../../assets/background/atomic.png";
 import logo from "../../assets/logo/logos.svg"
 import "./ProfilePage.scss";
+import { useTranslation } from "react-i18next";
 
-const ProfilePage: React.FC = () => (
-    <div className="profile">
-        {/* Mobile header - hidden on desktop */}
-        <div className="profile__header only_mobile">
-            <img className="profile__logo" src={logo} alt="game logo" />
-            <p className="profile__header__text">
-                Для активации DLC нужна основная игра на аккаунте
-            </p>
+const ProfilePage: React.FC = () => {
+    const { t } = useTranslation();
+    return (
+        <div className="profile">
+            {/* Mobile header - hidden on desktop */}
+            <div className="profile__header only_mobile">
+                <img className="profile__logo" src={logo} alt="game logo" />
+                <p className="profile__header__text">
+                    {t("profilePage.headerText")}
+                </p>
+            </div>
+
+            {/* Desktop logo - hidden on mobile */}
+            <img className="profile__logo mobile_hidden" src={logo} alt="game logo" />
+
+            <div className="profile__container">
+                <ProfileLeft
+                    imgSrc={AtomicPng}
+                    title="Command & Conquer™ Red Alert™ 3- Uprising"
+                    orderId="99999999"
+                    dlcLabel="DLC"
+                    activationTime="00:00:00"
+                />
+
+                <ProfileRight/>
+            </div>
         </div>
-
-        {/* Desktop logo - hidden on mobile */}
-        <img className="profile__logo mobile_hidden" src={logo} alt="game logo" />
-
-        <div className="profile__container">
-            <ProfileLeft
-                imgSrc={AtomicPng}
-                title="Command & Conquer™ Red Alert™ 3- Uprising"
-                orderId="99999999"
-                dlcLabel="DLC"
-                activationTime="00:00:00"
-            />
-
-            <ProfileRight/>
-        </div>
-    </div>
-);
+    );
+};
 
 export default ProfilePage;
